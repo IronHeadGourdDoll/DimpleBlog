2f9b309d78a946279f4b3b8e12e6c3b2fd39d269
17264 <1726436515@qq.com>
2019-09-13 14:28:18 +0800

    final version

--- src/main/java/com/dimple/RedisTest.java
+++ src/main/java/com/dimple/RedisTest.java
@@ -1,7 +1,10 @@
 package com.dimple;
 
 public class RedisTest {
-    public static void main(String[] args) {
-
+    public static void main1(String[] args) {
+        //JedisShardInfo shardInfo = new JedisShardInfo("redis://localhost:6379/9");//这里是连接的本地地址和端口
+        //shardInfo.setPassword("123456");//这里是密码
+        //Jedis jedis = new Jedis(shardInfo);
+        //jedis.connect();
     }
 }
